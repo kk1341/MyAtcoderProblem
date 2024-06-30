7d77deda3770a404b8052dbacf2065dd3daa0498
kk1341 <kazz9276@gmail.com>
2024-06-30 18:34:50 +0900

    部屋の番号がずれていたのでそこだけ注意

--- abc265/b/main.py
+++ abc265/b/main.py
@@ -3,17 +3,15 @@ n, m, t = map(int, input().split())
 A = list(map(int, input().split()))
 X = [[int(x) for x in input().split()] for _ in range(m)]
 
-cnt = 1
-index = 0
-ans = 'Yes'
-while index < n:
-    t -= A[index]
-    if X[index][0] == cnt:
-        t += X[index][1]
-    if t <= 0:
-        ans = 'No'
-        break
-    cnt += 1
-    index += 1
+bouns_index = 0
+for i in range(n-1):
+    if t - A[i] > 0:
+        t -= A[i]
+        if bouns_index < m and X[bouns_index][0]-1 == i+1:
+            t += X[bouns_index][1]
+            bouns_index += 1
+    else:
+        print('No')
+        exit()
 
-print(ans)
+print('Yes')
