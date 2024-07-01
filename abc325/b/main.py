7a743b38478007e891c2dbbb53bdf722f79b4b53
kk1341 <kazz9276@gmail.com>
2024-07-02 00:07:23 +0900

    1時間ごとに参加可能な拠点の人数を更新して最大値を求める

--- abc325/b/main.py
+++ abc325/b/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+n = int(input())
+wx = [[int(x) for x in input().split()] for _ in range(n)]
+
+ans = 0
+for i in range(24):
+    human = 0
+    for j in range(n):
+        t = (i + wx[j][1]) % 24
+        if 9 <= t <= 17:
+            human += wx[j][0]
+    
+    ans = max(ans, human)
+
+print(ans)
