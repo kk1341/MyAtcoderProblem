7aca143e6ae22cc7576dfd7512b5351f32003612
kk1341 <kazz9276@gmail.com>
2024-07-02 10:03:10 +0900

    10**18までをカバーできるxとyの範囲を決め全探索

--- abc324/b/main.py
+++ abc324/b/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+n = int(input())
+
+for x in range(50):
+    for y in range(50):
+        if n == 2**x * 3**y:
+            print('Yes')
+            exit()
+
+print('No')
