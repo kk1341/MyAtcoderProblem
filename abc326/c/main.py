c826f69d31491b3e3c31966414f986d59f18652c
kk1341 <kazz9276@gmail.com>
2024-07-02 09:36:22 +0900

    二部探索を使ってx+Mが挿入できる場所を探す

--- abc326/c/main.py
+++ abc326/c/main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
+import bisect
+
 n, m = map(int, input().split())
-A = sorted(list(map(int, input().split())))
+A = list(map(int, input().split()))
+A.sort()
 
+ans = 0
+for i in range(n):
+    prezent = bisect.bisect_left(A, A[i]+m) - i
+    ans = max(ans, prezent)
 
+print(ans)
