563f6ee630538e920e13489c06a5b5581797e60a
kk1341 <kazz9276@gmail.com>
2024-07-02 16:23:08 +0900

    チーム番号と勝利数をセットで管理して勝利数でソート

--- abc323/b/main.py
+++ abc323/b/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+n = int(input())
+team = []
+
+for i in range(n):
+    s = input()
+    win = s.count('o')
+    team.append([i+1, win])
+
+team = sorted(team, key=lambda x: x[1], reverse=True)
+for i in range(n):
+    print(team[i][0], end=' ')
