916413412e23bcae83be4681d8510d9cd74814f5
kk1341 <kazz9276@gmail.com>
2024-07-02 17:48:56 +0900

    1からn日目と打ち上げる日の差をとる

--- abc322/c/main.py
+++ abc322/c/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+days = list(range(1, n+1))
+
+index = 0
+for i in range(n):
+    print(A[index]-days[i])
+    if days[i] == A[index]:
+        index += 1
