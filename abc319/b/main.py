8bbcd7ef58df50bed158e4bae2e2eb0c885a4b31
kk1341 <kazz9276@gmail.com>
2024-07-03 17:47:26 +0900

    nの約数を列挙し問題文通りの実装をする

--- abc319/b/main.py
+++ abc319/b/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+n = int(input())
+factors = []
+for i in range(1, 10):
+    if n % i == 0:
+        factors.append(i)
+
+ans = ''
+for i in range(n+1):
+    flag = False
+    for factor in factors:
+        if i % (n//factor) == 0:
+            flag = True
+            ans += str(factor)
+            break
+    if not flag:
+        ans += '-'
+
+print(ans)
