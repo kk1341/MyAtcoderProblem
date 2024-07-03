bcfee873feca750347d336b073c2118db80b0bb6
kk1341 <kazz9276@gmail.com>
2024-07-03 16:50:59 +0900

    最後の試合点数を全パターン試してx以上になるか

--- abc321/b/main.py
+++ abc321/b/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+n, x = map(int, input().split())
+A = list(map(int, input().split()))
+
+for last_score in range(0, 101):
+    all_score = A + [last_score]
+    all_score.sort()
+    all_score.pop(0)
+    all_score.pop(-1)
+    if sum(all_score) >= x:
+        print(last_score)
+        exit()
+
+print(-1)
