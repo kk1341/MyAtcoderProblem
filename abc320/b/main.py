e37fc6f27b273dddc4eea839238342d03c432fad
kk1341 <kazz9276@gmail.com>
2024-07-03 17:05:39 +0900

    部分列を全探索し回文かどうか判定

--- abc320/b/main.py
+++ abc320/b/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+s = input()
+ans = 0
+for i in range(len(s)):
+    for j in range(i+1, len(s)+1):
+        new_s = s[i:j]
+        if new_s == new_s[::-1]:
+            ans = max(ans, len(new_s))
+
+print(ans)
