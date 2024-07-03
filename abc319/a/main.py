ac84daf161540e58404926dd94ef6a46ea897ccb
kk1341 <kazz9276@gmail.com>
2024-07-03 17:27:32 +0900

    どれだけplayerの配列を速く書けるか

--- abc319/a/main.py
+++ abc319/a/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+s = input()
+player = [
+    ['tourist', 3858],
+    ['ksun48', 3679],
+    ['Benq', 3658],
+    ['Um_nik', 3648],
+    ['apiad', 3638],
+    ['Stonefeang', 3630],
+    ['ecnerwala', 3613],
+    ['mnbvmar', 3555],
+    ['newbiedmy', 3516], 
+    ['semiexp', 3481]
+]
+
+for i in range(10):
+    if s == player[i][0]:
+        print(player[i][1])
+        exit()
