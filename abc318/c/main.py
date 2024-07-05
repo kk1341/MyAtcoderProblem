2d4ab0e83f017b703c22c980c33843285ea451f7
kk1341 <kazz9276@gmail.com>
2024-07-05 11:28:46 +0900

    降順にソートして間隔dで運賃の総和がpに対してどうなるか

--- abc318/c/main.py
+++ abc318/c/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+n, d, p = map(int, input().split())
+F = sorted(list(map(int, input().split())), reverse=True)
+
+A = []
+index = 0
+while True:
+    if p < sum(F[index:index+d]):
+        A.append(p)
+    else:
+        A += F[index:]
+        break
+
+    index += d
+
+print(sum(A))    
