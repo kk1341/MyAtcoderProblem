fc06e2b9902403d13fdbce0f2b49889cc2edbc24
kk1341 <kazz9276@gmail.com>
2024-07-05 11:39:41 +0900

    条件を満たす最小値を探して添え字番号+1を出力

--- abc317/a/main.py
+++ abc317/a/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+n, h, x = map(int, input().split())
+P = list(map(int, input().split()))
+
+ans = 1000
+for p in P:
+    if x <= h + p:
+        ans = min(ans, p)
+
+print(P.index(ans)+1)
