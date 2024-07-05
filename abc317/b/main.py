16eea8a4fad06444b756d685b67f3346a4bef098
kk1341 <kazz9276@gmail.com>
2024-07-05 11:53:39 +0900

    与えらえた数列をソートして連続していない場所を探す

--- abc317/b/main.py
+++ abc317/b/main.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+n = int(input())
+A = sorted(list(map(int, input().split())))
+start = min(A)
+end = max(A)
+
+for i in range(start, end+1):
+    if A[i-start] != i:
+        print(i)
+        exit()
