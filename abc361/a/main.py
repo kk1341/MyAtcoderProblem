7f60528187adb287a5dcefeac591ed0af06e4430
kk1341 <kazz9276@gmail.com>
2024-07-06 23:12:34 +0900

    配列の前からforぶん回してk番目になったら挿入

--- abc361/a/main.py
+++ abc361/a/main.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+n, k, x = map(int, input().split())
+A = list(map(int, input().split()))
+
+ans = []
+for i in range(n):
+    if i == k:
+        ans.append(x)
+    ans.append(A[i])
+
+if n == k:
+    ans.append(x)
+print(*ans)
