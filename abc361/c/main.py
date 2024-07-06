1bef0893a65251721ead2be116e7c647d093ba09
kk1341 <kazz9276@gmail.com>
2024-07-06 23:15:17 +0900

    ソートした配列を受け取ってk個ずつ差をとっていった時の最小値が答え

--- abc361/c/main.py
+++ abc361/c/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+
+n, k = map(int, input().split())
+A = sorted(list(map(int, input().split())))
+
+index = 0
+a = n - k - 1
+ans = 10**9
+while index + a < n:
+    ans = min(A[index+a] - A[index], ans)
+    index += 1
+
+print(ans)
