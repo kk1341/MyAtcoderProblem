cbfe7adc66f26d9af5196a3d743c1f296453e054
kk1341 <kazz9276@gmail.com>
2024-07-06 23:14:09 +0900

    一次元の問題に落とし込んで、軸ごとに座標の位置関係を調べる

--- abc361/b/main.py
+++ abc361/b/main.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+a, b, c, d, e, f = map(int, input().split())
+g, h, i, j, k, l = map(int, input().split())
+
+x1_min, x1_max = min(a, d), max(a, d)
+y1_min, y1_max = min(b, e), max(b, e)
+z1_min, z1_max = min(c, f), max(c, f)
+
+x2_min, x2_max = min(g, j), max(g, j)
+y2_min, y2_max = min(h, k), max(h, k)
+z2_min, z2_max = min(i, l), max(i, l)
+
+if x1_min < x2_max and x2_min < x1_max and y1_min < y2_max and y2_min < y1_max and z1_min < z2_max and z2_min < z1_max :
+    print('Yes')
+else:
+    print('No')
