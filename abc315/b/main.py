104713d9af086e56e157b2fe93fd9137f8ecdf62
kk1341 <kazz9276@gmail.com>
2024-07-08 14:11:07 +0900

    midから各月の日数を引いて行ってifの条件が成り立てば中央の月と日付になっている

--- abc315/b/main.py
+++ abc315/b/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+m = int(input())
+D = list(map(int, input().split()))
+mid = (sum(D)+1) // 2
+
+for i in range(m):
+    if mid <= D[i]:
+        print(i+1, mid)
+        exit()
+    else:
+        mid -= D[i]
