177fffc221c8747abae4ffc337e290ee335ce251
kk1341 <kazz9276@gmail.com>
2024-07-19 01:07:06 +0900

    全探索するだけ

--- abc351/b/main.py
+++ abc351/b/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+n = int(input())
+A = [input() for _ in range(n)]
+B = [input() for _ in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        s1, s2 = A[i], B[i]
+        if s1[j] != s2[j]:
+            print(i+1, j+1)
+            exit()
