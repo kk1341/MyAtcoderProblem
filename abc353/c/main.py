050f57d07717c4b39cb2798a3750c1aee78cc6e4
kk1341 <kazz9276@gmail.com>
2024-07-18 21:33:35 +0900

    しゃくとり法を用いて10**8を超える2つの要素の和を数える

--- abc353/c/main.py
+++ abc353/c/main.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+n = int(input())
+A = sorted(list(map(int, input().split())))
+
+r = n
+cnt, ans = 0, 0
+for i in range(n):
+    r = max(r, i+1)
+    while r - 1 > i and A[i] + A[r-1] >= 10**8:
+        r -= 1
+    cnt += (n - r)
+
+for i in range(n):
+    ans += (n-1) * A[i]
+
+ans -= 10**8 * cnt
+print(ans)
