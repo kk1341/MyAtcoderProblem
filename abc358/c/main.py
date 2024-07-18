ce9278ccd8ba6ae0f147186d97310ee187497eb4
kk1341 <kazz9276@gmail.com>
2024-07-18 13:09:12 +0900

    制約が小さいのでbit全探索で最小値を探索

--- abc358/c/main.py
+++ abc358/c/main.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+n, m = map(int, input().split())
+S = [[s for s in input()] for _ in range(n)]
+
+ans = n
+for i in range(2**n):
+    exist = [False] * m
+    cnt = 0
+    for j in range(n):
+        if (i >> j) & 1:
+            cnt += 1
+            for k in range(m):
+                if S[j][k] == 'o':
+                    exist[k] = True
+    
+    all_exist = True
+    for j in range(m):
+        if not exist[j]:
+            all_exist = False
+    if all_exist:
+        ans = min(ans, cnt)
+
+print(ans)
