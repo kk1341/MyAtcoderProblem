b9475b49d844c179f6f6cf679da85f099f3502ef
kk1341 <kazz9276@gmail.com>
2024-07-20 12:59:50 +0900

    sかsの逆順がすでにあるかを判定する．setを使うと良し

--- abc310/c/main.py
+++ abc310/c/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+n = int(input())
+S = set()
+
+for i in range(n):
+    s = input()
+    if s in S or s[::-1] in S:
+        continue
+    else:
+        S.add(s)
+
+print(len(S))
