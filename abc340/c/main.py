5d9b457b38719401c47e5fc0c3750d39855a2c5d
kk1341 <kazz9276@gmail.com>
2024-07-22 13:18:03 +0900

    メモ化再帰で解く．pythonはcacheを使うと良い

--- abc340/c/main.py
+++ abc340/c/main.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+from functools import cache
+
+@cache
+def solve(n):
+    if n == 1:
+        return 0
+    else:
+        return solve(n//2) + solve((n+1)//2) + n
+
+print(solve(int(input())))
