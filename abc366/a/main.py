b71d0205ea3355c32c3abf348092c6428e5347a9
kk1341 <kazz9276@gmail.com>
2024-08-31 17:16:26 +0900

    最初にどちらが勝っているかを判定し、残りの票を足してどうなるか

--- abc366/a/main.py
+++ abc366/a/main.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+n, t, a = map(int, input().split())
+still = n - (t+a)
+if t > a:
+    if t > a + still:
+        print('Yes')
+    else:
+        print('No')
+else:
+    if a > t + still:
+        print('Yes')
+    else:
+        print('No')
