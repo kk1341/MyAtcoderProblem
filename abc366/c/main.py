14a2b685e1b048082cc66e9da4b101a1730bf9a5
kk1341 <kazz9276@gmail.com>
2024-09-15 12:47:29 +0900

    入力がばらつくときは* を使ってリストで受け取る

--- abc366/c/main.py
+++ abc366/c/main.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+
+q = int(input())
+porch = defaultdict(int)
+ans = 0
+
+for _ in range(q):
+    t, *x = map(int, input().split())
+    if t == 1:
+        porch[x[0]] += 1
+        if porch[x[0]] == 1:
+            ans += 1
+    elif t == 2:
+        porch[x[0]] -= 1
+        if porch[x[0]] == 0:
+            ans -= 1
+    else:
+        print(ans)
